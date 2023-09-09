a049d99f001be7f2725a57f51550df0e11f799e0
ArchLeaders <archleadership28@gmail.com>
2023-09-09 13:30:16 -0700

    Embed pdfium windows dll

--- src/Services/ChromiumExporterService.cs
+++ src/Services/ChromiumExporterService.cs
@@ -2,11 +2,32 @@
 using PdfiumViewer;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 
 namespace PdfConverter.Services;
 
 public class ChromiumExporterService : IExporterService
 {
+    private static readonly string _dll = Path.Combine(Path.GetTempPath(), "pdfium.dll");
+
+    static ChromiumExporterService()
+    {
+        if (!File.Exists(_dll)) {
+            if (typeof(Program).Assembly.GetManifestResourceStream("PdfConverter.Resources.pdfium.dll") is Stream stream) {
+                using (FileStream fs = File.Create(_dll)) {
+                    stream.CopyTo(fs);
+                }
+
+                stream.Dispose();
+            }
+            else {
+                throw new FileNotFoundException("Could not find embedded pdfium!");
+            }
+        }
+
+        NativeLibrary.Load(_dll);
+    }
+
     public static float DPI { get; set; } = 72;
 
     public void Export(string path, string output, ImageFormat imageFormat, int rescale)
