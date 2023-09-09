81eef02f6c5812a27bc996755510ff2a63bccf5c
ArchLeaders <archleadership28@gmail.com>
2023-09-09 13:21:35 -0700

    Add double-click to browse features

--- src/Views/ShellView.axaml.cs
+++ src/Views/ShellView.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Platform.Storage;
+using PdfConverter.Helpers;
 using PdfConverter.ViewModels;
 
 namespace PdfConverter.Views;
@@ -11,18 +12,33 @@ public partial class ShellView : Window
     {
         InitializeComponent();
         FileList.AddHandler(DragDrop.DropEvent, FileListDropEvent);
+        FileList.DoubleTapped += FileList_DoubleTapped;
+    }
+
+    private async void FileList_DoubleTapped(object? sender, TappedEventArgs e)
+    {
+        BrowserDialog dialog = new(BrowserMode.OpenFile, "Select PDF Files", filter: "PDF Files:*.pdf|All Files:*.*", instanceBrowserKey: "select-pdf-files");
+        if (await dialog.ShowDialog(allowMultiple: true) is IEnumerable<string> paths) {
+            AddFiles(paths);
+        }
     }
 
     public void FileListDropEvent(object? sender, DragEventArgs e)
+    {
+
+        if (e.Data.GetFiles() is IEnumerable<IStorageItem> paths) {
+            AddFiles(paths.Select(x => x.Path.LocalPath));
+        }
+    }
+
+    private void AddFiles(IEnumerable<string> paths)
     {
         if (DataContext is not ShellViewModel svm) {
             return;
         }
 
-        if (e.Data.GetFiles() is IEnumerable<IStorageItem> paths) {
-            foreach (var path in paths.Select(x => x.Path.LocalPath).Where(x => x.ToLower().EndsWith(".pdf") && !svm.Files.Select(x => x.FilePath).Contains(x))) {
-                svm.Files.Add(new(path));
-            }
+        foreach (var path in paths.Where(x => x.ToLower().EndsWith(".pdf") && !svm.Files.Select(x => x.FilePath).Contains(x))) {
+            svm.Files.Add(new(path));
         }
     }
 }
